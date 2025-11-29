0226264c4ee178534b890260fe2e700e010f49b0
Nokeyang <geeksatyam@gmail.com>
2025-11-29 21:17:50 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Contact Form
+const form = document.getElementById("contactForm");
+const msg = document.getElementById("formMsg");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+
+  if (!name || !email) {
+    msg.textContent = "Please fill out required fields.";
+    msg.style.color = "red";
+    return;
+  }
+
+  msg.textContent = "Message sent successfully!";
+  msg.style.color = "lightgreen";
+  form.reset();
+});
+
+// Footer year
+document.getElementById("year").textContent = new Date().getFullYear();
